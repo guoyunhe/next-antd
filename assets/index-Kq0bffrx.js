0f826c19ff0adfdde27a2d5053ec13a35439b38f
guoyunhe <guoyunhe@users.noreply.github.com>
2024-02-14 12:51:51 +0000

    deploy: 360542bf9e02bdad5f36ba7b6bd5d1941485d160

--- assets/index-Kq0bffrx.js
+++ assets/index-Kq0bffrx.js
@@ -679,36 +679,44 @@ import { Tag } from 'antd';
 `})}),`
 `,A.jsx(t.h2,{children:"size"}),`
 `,A.jsx(t.p,{children:"Ant Design 的标签不支持 size，只能自定义类实现。"})]})}function pL(e={}){const{wrapper:t}={...Cn(),...e.components};return t?A.jsx(t,{...e,children:A.jsx(UE,{...e})}):UE(e)}pL.displayName="MDXContent";const Tle=Object.freeze(Object.defineProperty({__proto__:null,default:pL,filepath:Ile,frontmatter:kle,title:Cle},Symbol.toStringTag,{value:"Module"})),$le="docs/Timeline.md",Nle="Timeline",Rle=void 0;function qE(e){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...Cn(),...e.components};return A.jsxs(A.Fragment,{children:[A.jsx(t.h1,{children:"Timeline"}),`
-`,A.jsx(t.h2,{children:"Item content"}),`
-`,A.jsx(t.p,{children:"Fusion Design 的 Timeline.Item content 对应 Ant Design 的 Timeline.Item children。"}),`
+`,A.jsx(t.h2,{children:"Item content, title, time"}),`
+`,A.jsx(t.p,{children:"Fusion Design 的 Timeline.Item 支持 content title time 三个字段来传入信息。Ant Design 的 Timeline.Item 则只支持 label （时间线左边）和 children（时间线右边）。在实际使用中，最简单的替换方式是直接使用 children 并自己组织布局样式。"}),`
 `,A.jsx(t.pre,{imports:{Timeline:FE},children:A.jsx(t.code,{className:"language-jsx",children:`// Fusion Design
 import { Timeline } from '@alifd/next';
 
 <Timeline>
-  <Timeline.Item content="描述" />
+  <Timeline.Item title="标题" content="描述" time="2024-02-18" />
 </Timeline>;
 `})}),`
 `,A.jsx(t.pre,{imports:{Timeline:qS},children:A.jsx(t.code,{className:"language-jsx",children:`// Ant Design
 import { Timeline } from 'antd';
 
 <Timeline>
-  <Timeline.Item>描述</Timeline.Item>
+  <Timeline.Item>
+    <div className="my-title">标题</div>
+    <div className="my-content">描述</div>
+    <div className="my-time">2024-02-18</div>
+  </Timeline.Item>
 </Timeline>;
 `})}),`
-`,A.jsx(t.h2,{children:"Item title"}),`
-`,A.jsx(t.p,{children:"Fusion Design 的 Timeline.Item title 对应 Ant Design 的 Timeline.Item label。"}),`
+`,A.jsx(t.h2,{children:"Item state"}),`
+`,A.jsx(t.p,{children:"Fusion Design 的 Timeline.Item 用 state 属性来控制节点的颜色。对应 Ant Design 的 Timeline.Item color。"}),`
 `,A.jsx(t.pre,{imports:{Timeline:FE},children:A.jsx(t.code,{className:"language-jsx",children:`// Fusion Design
 import { Timeline } from '@alifd/next';
 
 <Timeline>
-  <Timeline.Item title="标题" content="描述" />
+  <Timeline.Item content="描述" state="process" />
+  <Timeline.Item content="描述" state="success" />
+  <Timeline.Item content="描述" state="done" />
 </Timeline>;
 `})}),`
 `,A.jsx(t.pre,{imports:{Timeline:qS},children:A.jsx(t.code,{className:"language-jsx",children:`// Ant Design
 import { Timeline } from 'antd';
 
 <Timeline>
-  <Timeline.Item label="标题">描述</Timeline.Item>
+  <Timeline.Item color="blue">描述</Timeline.Item>
+  <Timeline.Item color="green">描述</Timeline.Item>
+  <Timeline.Item color="gray">描述</Timeline.Item>
 </Timeline>;
 `})})]})}function dL(e={}){const{wrapper:t}={...Cn(),...e.components};return t?A.jsx(t,{...e,children:A.jsx(qE,{...e})}):qE(e)}dL.displayName="MDXContent";const Ale=Object.freeze(Object.defineProperty({__proto__:null,default:dL,filepath:$le,frontmatter:Rle,title:Nle},Symbol.toStringTag,{value:"Module"}));function Lle(){return A.jsx(d7,{docs:[m7,w7,sle,Ole,gle,xle,Ele,Tle,Ale],basename:"/next-antd/",languages:void 0})}ik(document.getElementById("root")).render(A.jsx(Lle,{}));
 function __vite__mapDeps(indexes) {
